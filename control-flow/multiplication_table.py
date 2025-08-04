d7738f1d0224e74cda14664f2ec1067e5c2b3a43
khansae <khansaeugc18@gmail.com>
2025-08-04 13:50:10 +0200

    Multiplication_table

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,14 @@
+# multiplication_table.py
+
+try:
+    # Prompt user input
+    number = int(input("Enter a number to see its multiplication table: "))
+
+    # Loop from 1 to 10
+    for i in range(1, 11):
+        result = number * i
+        print(f"{number} * {i} = {result}")
+
+except ValueError:
+    print("Please enter a valid integer.")
+
